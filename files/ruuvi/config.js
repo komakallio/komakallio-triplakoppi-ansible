568f14b08f376c766ff8d6295a560a23a881a064
Jari Saukkonen <jari.saukkonen@taika.fi>
2019-08-26 22:20:48 +0300

    Update park position

--- files/ruuvi/config.js
+++ files/ruuvi/config.js
@@ -4,7 +4,7 @@ module.exports = {
         'e50e423063a3': 'samuli'
     },
     parkPositions: {
-        'c331da55f7b1': [-1022, 146, 13],
+        'c331da55f7b1': [450, 935, -23],
         'e50e423063a3': [948, 225, 57]
     }
 }
